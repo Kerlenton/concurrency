9fc684978238ec97d81db12845909c14d99e2281
Kerlenton <semyonschv@yandex.ru>
2024-01-27 15:16:28 +0300

    Increment std::atomic variable 2000 times, get 2000 as expected

---
+++ ver21.cc
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <thread>
+#include <atomic>
+
+std::atomic<int> counter{0};
+
+void some_thread() {
+    for (unsigned i = 0; i < 1000; i++) {
+        ++counter;
+        std::cout << '\b';
+    }
+}
+
+int main() {
+    std::thread t1(some_thread), t2(some_thread);
+    t1.join();
+    t2.join();
+    std::cout << "counter = " << counter << std::endl;
+}
